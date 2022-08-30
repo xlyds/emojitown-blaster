f5d515c791ab9e7196d75dfa0f269d96dc975883
ltidwell <ztidwell@planview.com>
2022-08-30 18:38:28 -0500

    example user 💤

--- users.mjs
+++ users.mjs
@@ -1,36 +1,9 @@
 const users = [
-  {
-    "username": "test.user@leankit.com",
-    "password": "test1234"
-  },
-  {
-    "username": "jacquelyn.scree@leankit.com",
-    "password": "test1234"
-  },
-  {
-    "username": "sierra.talus@leankit.com",
-    "password": "test1234"
-  },
-  {
-    "username": "barbara.hopfield@leankit.com",
-    "password": "test1234"
-  },
-  {
-    "username": "diane.karsthill@leankit.com",
-    "password": "test1234"
-  },
-  {
-    "username": "lev.atmos@leankit.com",
-    "password": "test1234"
-  },
-  {
-    "username": "carter.blackland@leankit.com",
-    "password": "test1234"
-  },
-  {
-    "username": "skyler.mesa@leankit.com",
-    "password": "test1234"
-  }
+  // Add other users here
+  // {
+  //   "username": "sierra.talus@leankit.com",
+  //   "password": "test1234"
+  // }
 ];
 
 export default users;
